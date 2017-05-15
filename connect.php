f2acde46101bf7ba3541e94b93d359ae16b3f5a1
Ahmed Viquas <ahmedviquas105@gmail.com>
2017-05-15 12:06:02 +0530

    Removed sensitive information

--- connect.php
+++ connect.php
@@ -3,10 +3,10 @@
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
 
-$servername = "internal-db.s112684.gridserver.com";
-$username = "db112684_vicky";
-$password = "Treeback89#$";
-$dbname = "db112684_yw8_data";
+// $servername = "";
+// $username = "";
+// $password = "";
+// $dbname = "";
 // date_default_timezone_set('Asia/Kolkata');
 
 $conn = new mysqli($servername, $username, $password, $dbname);
